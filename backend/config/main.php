5b4a795ae3ab7df29f5a54834956f19be4e2e82f
1 <1>
2018-01-08 16:04:41 +0800

    111111111111

--- backend/config/main.php
+++ backend/config/main.php
@@ -12,7 +12,7 @@ return [
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
     'modules' => [],
-    'language'=>'Zh-CN',
+    'language'=>'zh-CN',
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
